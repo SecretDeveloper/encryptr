3588eab3aae85abc89dc57b8733aefb8370b841f
Gary Kenneally <garykenneally@gmail.com>
2017-11-04 18:26:06 +0000

    0.1.2

--- src/Encryptr.cs
+++ src/Encryptr.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
+using System.Collections.Generic;
 
 namespace Encryptr
 {
@@ -27,33 +28,38 @@ namespace Encryptr
             if (string.IsNullOrEmpty(plainText)) throw new ArgumentNullException("plainText");
             if (string.IsNullOrEmpty(password)) throw new ArgumentNullException("password");
 
-            // Derive a new Salt and IV from the Key
+            byte[] salt;
+            byte[] key;
+            byte[] iv;
+            
+            // Create new pseudo random salt, key and iv using password.
             using (var keyDerivationFunction = new Rfc2898DeriveBytes(password, _saltLength, _iterations))
             {
-                byte[] salt = keyDerivationFunction.Salt;
-                byte[] key = keyDerivationFunction.GetBytes(_keyLength);
-                byte[] iv = keyDerivationFunction.GetBytes(_ivLength);
-                byte[] checksum = new byte[_checksumLength];
-
-                // get sha256 of unencrypted content.
-                using(var sha = new SHA256Managed())
-                    checksum = sha.ComputeHash(Encoding.Unicode.GetBytes(plainText));
-                
-                using (var aesManaged = new AesManaged(){Padding = PaddingMode.PKCS7})
-                using (var encryptor = aesManaged.CreateEncryptor(key, iv))
-                using (var memoryStream = new MemoryStream())
-                {
-                    using (var cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write))
-                    using (var streamWriter = new StreamWriter(cryptoStream))
-                    {
-                        streamWriter.Write(plainText);
-                    }
-
-                    var cipher = memoryStream.ToArray();                                        
-                    salt = salt.Concat(checksum).Concat(cipher).ToArray(); // concat all the bytes!
-                    return salt;
-                }
+                salt = keyDerivationFunction.Salt;
+                key = keyDerivationFunction.GetBytes(_keyLength);
+                iv = keyDerivationFunction.GetBytes(_ivLength);                
             }
+
+            // get sha256 of unencrypted content.
+            byte[] checksum = new byte[_checksumLength];
+            using(var sha = new SHA256Managed())
+                checksum = sha.ComputeHash(Encoding.Unicode.GetBytes(plainText));
+            
+            // encrypt the content using AES256
+            byte[] cipher;
+            using (var aes = new AesManaged(){Padding = PaddingMode.PKCS7, KeySize=256})
+            using (var encryptor = aes.CreateEncryptor(key, iv))
+            using (var memoryStream = new MemoryStream())
+            {
+                using (var cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write))
+                using (var streamWriter = new StreamWriter(cryptoStream))
+                {
+                    streamWriter.Write(plainText);  // Write our text through cryptostream into encrypted content.
+                }
+                cipher = memoryStream.ToArray(); // get our encrypted content.
+            }            
+
+            return salt.Concat(checksum).Concat(cipher).ToArray(); // concat all the bytes!            
         }
 
         public static string Decrypt(byte[] input, string password)
@@ -61,44 +67,41 @@ namespace Encryptr
             if (input.Length < _saltLength + _checksumLength) throw new ArgumentNullException("input");
             if (string.IsNullOrEmpty(password)) throw new ArgumentNullException("password");
 
-            // Extract the salt from our ciphertext            
+            // bytestream contains: [salt][checksum][cipher]
             byte[] salt = input.Take(_saltLength).ToArray();
             byte[] checksum = input.Skip(_saltLength).Take(_checksumLength).ToArray();
-            byte[] cipher = input.Skip(_saltLength+_checksumLength)
-                                            .Take(input.Length - (_saltLength+_checksumLength))
-                                            .ToArray();
+            var cipher = input.Skip(_saltLength+_checksumLength).ToArray();
 
+            // Derive Key and IV from salt.
+            byte[] key;
+            byte[] iv;
             using (var keyDerivationFunction = new Rfc2898DeriveBytes(password, salt, _iterations))
-            {
-                // Derive the previous IV from the Key and Salt
-                byte[] keyBytes = keyDerivationFunction.GetBytes(_keyLength);
-                byte[] ivBytes = keyDerivationFunction.GetBytes(_ivLength);
-                
-                // Create a decrytor to perform the stream transform.
-                // Create the streams used for decryption.
-                // The default Cipher Mode is CBC and the Padding is PKCS7 which are both good
-                using (var aesManaged = new AesManaged(){Padding = PaddingMode.PKCS7})
-                using (var decryptor = aesManaged.CreateDecryptor(keyBytes, ivBytes))
-                using (var memoryStream = new MemoryStream(cipher))
-                using (var cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read))
-                using (var streamReader = new StreamReader(cryptoStream))
-                {
-                    // Return the decrypted bytes from the decrypting stream.
-                    var plain = streamReader.ReadToEnd();
-                    
-                    // Hash content
-                    using(var sha = new SHA256Managed()){
-                        byte[] contentChecksum = sha.ComputeHash(Encoding.Unicode.GetBytes(plain));
-                        var expectedChecksum = ByteArrayToString(checksum);
-                        var actualChecksum = ByteArrayToString(contentChecksum);
-                        if(expectedChecksum.Length == _checksumLength && expectedChecksum != actualChecksum) 
-                            throw new ApplicationException(
-                                string.Format("ERROR: Invalid checksum found, content cannot be decrypted.  Expected '{0}' but found '{1}'", expectedChecksum, actualChecksum));
-                    }
-
-                    return plain;
-                }
+            {                
+                key = keyDerivationFunction.GetBytes(_keyLength);
+                iv = keyDerivationFunction.GetBytes(_ivLength);
             }
+
+            // Decrypt content using AES256 
+            string plain;
+            using (var aes = new AesManaged(){Padding = PaddingMode.PKCS7, KeySize=256})
+            using (var decryptor = aes.CreateDecryptor(key, iv))
+            using (var memoryStream = new MemoryStream(cipher))
+            using (var cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read))
+            using (var streamReader = new StreamReader(cryptoStream))
+            {                
+                plain = streamReader.ReadToEnd(); // Grab decrypted string
+            }
+                
+            // Check checksum
+            byte[] contentChecksum;
+            using(var sha = new SHA256Managed())
+                contentChecksum = sha.ComputeHash(Encoding.Unicode.GetBytes(plain));
+                            
+            if(contentChecksum.Length != _checksumLength || (ByteArrayToString(checksum) != ByteArrayToString(contentChecksum)))
+                throw new ApplicationException(string.Format("ERROR: Invalid checksum found, content cannot be decrypted.  Expected '{0}' but found '{1}'"
+                                                            , ByteArrayToString(checksum), ByteArrayToString(contentChecksum)));
+        
+            return plain;// return our decrypted content.                    
         }        
     }
 }
