5344fb6279b56c1c743d233004e72470a95c5f62
Gary Kenneally <garykenneally@gmail.com>
2017-11-04 17:42:29 +0000

    0.1.1

--- README.md
+++ README.md
@@ -1,5 +1,46 @@
 # Encryptr
-Encryptr is a simple text and file encryption/decryption tool which uses RFC
+Encryptr is a simple text and file encryption/decryption tool which uses RFC2898 (AES256).
 
 [<img src="https://img.shields.io/appveyor/ci/secretdeveloper/encryptr/master.svg">](https://ci.appveyor.com/project/SecretDeveloper/encryptr)
 [<img src="https://img.shields.io/nuget/v/encryptr.svg">](https://www.nuget.org/packages/encryptr/)
+
+
+```
+Encryptr 
+
+Description:
+    AES256 encryption tool.  Encrypted content is encoded as base64.    
+
+Syntax:
+The following argument prefix characters can be used: '-','/'
+    --password, -p    
+        The password used to encrypt and decrypt your content, do not 
+        forget it or all is lost!
+        Required, Default:''
+        
+    --text, -t    
+        The text to be encrypted or decrypted.
+        [Optional], Default:''
+        
+    --decrypt, -d    
+        Decrypt flag
+        [Optional], Default:''
+        
+    --input, -i    
+        
+        [Optional], Default:'Path to input FILE to be encrypted or decrypted.'
+        
+    --output, -o    
+        
+        [Optional], Default:'Path to output FILE where content will be 
+        written,  if ommitted content is written to STDOUT.'
+        
+
+Examples:
+    Encryption:
+    encryptr 'mypassword' 'my content to encrypt'
+    Decryption:
+    encryptr 'mypassword' 'BASE64_STRING' -d
+```
+
+
--- src/EncryptrCommand.cs
+++ src/EncryptrCommand.cs
@@ -3,26 +3,31 @@ using CliParse;
 
 namespace Encryptr
 {
-    [ParsableClass("Encryptr", "Simple encryption tool")]
+    [ParsableClass("Encryptr", "AES256 encryption tool.  Encrypted content is encoded as base64.",
+    FooterText=@"Examples:
+    Encryption:
+    encryptr 'mypassword' 'my content to encrypt'
+    Decryption:
+    encryptr 'mypassword' 'BASE64_STRING' -d ")]
     internal class EncryptrCommand:Parsable
     {
         public EncryptrCommand()
         {
         }
 
-        [ParsableArgument("password", ShortName = 'p', DefaultValue = "", ImpliedPosition = 1, Required = true)]
+        [ParsableArgument("password", ShortName = 'p', DefaultValue = "", ImpliedPosition = 1, Required = true, Description="The password used to encrypt and decrypt your content, do not forget it or all is lost!")]
         public string Password { get; set; }
                 
-        [ParsableArgument("text", ShortName = 't', DefaultValue = "", ImpliedPosition = 2, Required = false)]
+        [ParsableArgument("text", ShortName = 't', DefaultValue = "", ImpliedPosition = 2, Required = false, Description="The text to be encrypted or decrypted.")]
         public string Text { get; set; }
 
-        [ParsableArgument("decrypt", ShortName = 'd')]
+        [ParsableArgument("decrypt", ShortName = 'd', Description="Decrypt flag")]
         public bool Decrypt { get; set; }
 
-        [ParsableArgument("input", ShortName = 'i', DefaultValue = "")]
+        [ParsableArgument("input", ShortName = 'i', DefaultValue = "Path to input FILE to be encrypted or decrypted.")]
         public string InputFile { get; set; }
 
-        [ParsableArgument("output", ShortName = 'o', DefaultValue = "")]
+        [ParsableArgument("output", ShortName = 'o', DefaultValue = "Path to output FILE where content will be written,  if ommitted content is written to STDOUT.")]
         public string OutputFile { get; set; }
 
         /// Hacky way to parse command line but whatever...
